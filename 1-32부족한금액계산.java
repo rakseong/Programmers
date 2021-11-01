e4883c9d1590a7d439a7499f06a180808ce674f8
rakseong <90314168+rakseong@users.noreply.github.com>
2021-11-01 13:41:44 +0900

    1-32부족한금액계산

---
+++ 1-32부족한금액계산.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        for(int i =1; i<=count; i++){
+            answer += (price*i);
+        }
+
+        if(money-answer<0){
+            answer = answer-money;
+        }else
+            answer = 0;
+
+        return answer;
+    }
+}
