16d75f85cb94d92799231034c49c2ce0d7c6b7f0
rakseong <90314168+rakseong@users.noreply.github.com>
2022-12-08 19:17:20 +0900

    level1

---
+++ 1__22김서방 찾기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+        answer = "김서방은 "+Arrays.asList(seoul).indexOf("Kim")+"에 있다";
+        
+        return answer;
+    }
+}
